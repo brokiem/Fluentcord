a8be1e29b68c47ffe063a8380ce8431cf68de93b
brokiem <milyhqvideo@gmail.com>
2024-11-26 09:40:02 +0800

    Only load mock data when needed

--- src/Fluentcord/ViewModels/MainViewModel.cs
+++ src/Fluentcord/ViewModels/MainViewModel.cs
@@ -57,118 +57,38 @@ public partial class MainViewModel : ViewModelBase
                     _random.Next(-2, 3) > 0
                         ? $"https://cdn.discordapp.com/embed/avatars/{_random.Next(1, 5)}.png"
                         : null));
-
-                for (var j = 1; j <= _random.Next(15, 50); j++)
-                {
-                    var categoryId = CreateId();
-                    GuildChannels.Add(new CategoryChannelModel(categoryId, "Text Channels", [], j, guildId));
-
-                    for (var k = 1; k <= _random.Next(5, 15); k++)
-                    {
-                        var channelId = CreateId();
-                        GuildChannels.Add(new TextChannelModel(channelId, guildId, $"{channelId}", j + k, [], 0, false,
-                            "This is the channel topic", null, categoryId));
-
-                        for (var l = 1; l <= _random.Next(10, 50); l++)
-                        {
-                            var messageId = CreateId();
-                            var authorId = CreateId();
-                            Messages.Add(new MessageModel(
-                                messageId: messageId,
-                                channelId: channelId,
-                                author: new UserModel(
-                                    id: authorId,
-                                    username: $"{_faker.Name.FirstName()} {_faker.Name.LastName()}",
-                                    discriminator: 0,
-                                    globalName: null,
-                                    avatarUrl: $"https://cdn.discordapp.com/embed/avatars/{_random.Next(1, 5)}.png",
-                                    isBot: false,
-                                    isSystemUser: null,
-                                    mfaEnabled: null,
-                                    bannerUrl: null,
-                                    accentColor: null,
-                                    locale: null,
-                                    verified: null,
-                                    email: null,
-                                    flags: null,
-                                    premiumType: null,
-                                    publicFlags: null
-                                ),
-                                content: _faker.Rant.Review("Discord"),
-                                isEdited: false,
-                                createdAt: DateTimeOffset.Now,
-                                editedAt: null,
-                                mentionEveryone: false,
-                                mentions: [],
-                                attachments:
-                                [
-                                    new ImageAttachmentModel(1, "Attachment name", null, "image/png", 500,
-                                        "https://picsum.photos/536/354",
-                                        "https://picsum.photos/536/354",
-                                        false, null, 354, 536)
-                                ],
-                                embeds: [],
-                                pinned: false,
-                                webhookId: null,
-                                messageType: MessageType.Default,
-                                applicationId: null,
-                                flags: null,
-                                messageReference: new MessageReferenceModel(new MessageModel(
-                                    messageId: messageId,
-                                    channelId: channelId,
-                                    author: new UserModel(
-                                        id: authorId,
-                                        username: $"{_faker.Name.FirstName()} {_faker.Name.LastName()}",
-                                        discriminator: 0,
-                                        globalName: null,
-                                        avatarUrl: $"https://cdn.discordapp.com/embed/avatars/{_random.Next(1, 5)}.png",
-                                        isBot: false,
-                                        isSystemUser: null,
-                                        mfaEnabled: null,
-                                        bannerUrl: null,
-                                        accentColor: null,
-                                        locale: null,
-                                        verified: null,
-                                        email: null,
-                                        flags: null,
-                                        premiumType: null,
-                                        publicFlags: null
-                                    ),
-                                    content: _faker.Rant.Review("Discord"),
-                                    isEdited: false,
-                                    createdAt: DateTimeOffset.Now,
-                                    editedAt: null,
-                                    mentionEveryone: false,
-                                    mentions: [],
-                                    attachments: [],
-                                    embeds: [],
-                                    pinned: false,
-                                    webhookId: null,
-                                    messageType: MessageType.Default,
-                                    applicationId: null,
-                                    flags: null,
-                                    messageReference: null,
-                                    isReply: false
-                                )),
-                                isReply: true
-                            ));
-                        }
-                    }
-
-                    var voiceId = CreateId();
-                    GuildChannels.Add(new VoiceChannelModel(voiceId, null, "Voice channel", -1, categoryId, null, null,
-                        null, guildId, [], 0, false));
-
-                    // Voice channel member
-                    var memberId = CreateId();
-                    GuildChannels.Add(new VoiceChannelMemberModel(voiceId, memberId, _faker.Internet.UserName(),
-                        "https://cdn.discordapp.com/embed/avatars/4.png", true, true));
-                }
             }
         });
     }
 
-    private void LoadMembers()
+    private void LoadChannels(ulong guildId)
+    {
+        GuildChannels.Clear();
+
+        for (var j = 1; j <= _random.Next(15, 50); j++)
+        {
+            var categoryId = CreateId();
+            GuildChannels.Add(new CategoryChannelModel(categoryId, "Text Channels", [], j, guildId));
+
+            for (var k = 1; k <= _random.Next(5, 15); k++)
+            {
+                var channelId = CreateId();
+                GuildChannels.Add(new TextChannelModel(channelId, guildId, $"{channelId}", j + k, [], 0, false,
+                    "This is the channel topic", null, categoryId));
+            }
+
+            var voiceId = CreateId();
+            GuildChannels.Add(new VoiceChannelModel(voiceId, null, "Voice channel", -1, categoryId, null, null,
+                null, guildId, [], 0, false));
+
+            // Voice channel member
+            var memberId = CreateId();
+            GuildChannels.Add(new VoiceChannelMemberModel(voiceId, memberId, _faker.Internet.UserName(),
+                "https://cdn.discordapp.com/embed/avatars/4.png", true, true));
+        }
+    }
+
+    private void LoadMembers(ulong channelId)
     {
         Members.Clear();
 
@@ -194,13 +114,113 @@ public partial class MainViewModel : ViewModelBase
         }, DispatcherPriority.Background);
     }
 
+    private void LoadMessages(ulong channelId)
+    {
+        Messages.Clear();
+
+        for (var l = 1; l <= _random.Next(10, 50); l++)
+        {
+            var messageId = CreateId();
+            var authorId = CreateId();
+            Messages.Add(new MessageModel(
+                messageId: messageId,
+                channelId: channelId,
+                author: new UserModel(
+                    id: authorId,
+                    username: $"{_faker.Name.FirstName()} {_faker.Name.LastName()}",
+                    discriminator: 0,
+                    globalName: null,
+                    avatarUrl: $"https://cdn.discordapp.com/embed/avatars/{_random.Next(1, 5)}.png",
+                    isBot: false,
+                    isSystemUser: null,
+                    mfaEnabled: null,
+                    bannerUrl: null,
+                    accentColor: null,
+                    locale: null,
+                    verified: null,
+                    email: null,
+                    flags: null,
+                    premiumType: null,
+                    publicFlags: null
+                ),
+                content: _faker.Rant.Review("Discord"),
+                isEdited: false,
+                createdAt: DateTimeOffset.Now,
+                editedAt: null,
+                mentionEveryone: false,
+                mentions: [],
+                attachments:
+                [
+                    new ImageAttachmentModel(1, "Attachment name", null, "image/png", 500,
+                        "https://picsum.photos/536/354",
+                        "https://picsum.photos/536/354",
+                        false, null, 354, 536)
+                ],
+                embeds: [],
+                pinned: false,
+                webhookId: null,
+                messageType: MessageType.Default,
+                applicationId: null,
+                flags: null,
+                messageReference: new MessageReferenceModel(new MessageModel(
+                    messageId: messageId,
+                    channelId: channelId,
+                    author: new UserModel(
+                        id: authorId,
+                        username: $"{_faker.Name.FirstName()} {_faker.Name.LastName()}",
+                        discriminator: 0,
+                        globalName: null,
+                        avatarUrl: $"https://cdn.discordapp.com/embed/avatars/{_random.Next(1, 5)}.png",
+                        isBot: false,
+                        isSystemUser: null,
+                        mfaEnabled: null,
+                        bannerUrl: null,
+                        accentColor: null,
+                        locale: null,
+                        verified: null,
+                        email: null,
+                        flags: null,
+                        premiumType: null,
+                        publicFlags: null
+                    ),
+                    content: _faker.Rant.Review("Discord"),
+                    isEdited: false,
+                    createdAt: DateTimeOffset.Now,
+                    editedAt: null,
+                    mentionEveryone: false,
+                    mentions: [],
+                    attachments: [],
+                    embeds: [],
+                    pinned: false,
+                    webhookId: null,
+                    messageType: MessageType.Default,
+                    applicationId: null,
+                    flags: null,
+                    messageReference: null,
+                    isReply: false
+                )),
+                isReply: true
+            ));
+        }
+    }
+
     partial void OnSelectedGuildChanged(GuildModel? oldValue, GuildModel? newValue)
     {
+        if (newValue is null) return;
+
+        var guild = newValue;
+
+        LoadChannels(guild.Id);
     }
 
     partial void OnSelectedChannelChanged(ChannelModel? oldValue, ChannelModel? newValue)
     {
-        LoadMembers();
+        if (newValue is null) return;
+
+        var channel = newValue;
+
+        LoadMembers(channel.Id);
+        LoadMessages(channel.Id);
     }
 
     public void SelectGuild(GuildModel? guild)
